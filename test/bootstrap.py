a00c780edcfe3ff50e8843da86ed2410156511e9
jesusjorge <35180978+jesusjorge@users.noreply.github.com>
2023-07-17 09:02:04 -0700

    Update bootstrap.py

--- test/bootstrap.py
+++ test/bootstrap.py
@@ -1,7 +1,11 @@
 #https://pyscript.net/examples/repl.html
 #
 # Al ejecutar este codigo en el REPL de PyScript, Python descarga el contenido de un URL, e inmediatamente ejecuta su codigo
-#
+# En el caso de GitHub, no necesita ninguna consideración especial con CORS
+# No es el mismo caso con otros sitios (http://checkip.dyndns.org).
+#   Para tales propositos, se podria utilizar un proxy de CORS (https://corsproxy.io/)
+#   ej. https://corsproxy.io/?http%3A%2F%2Fcheckip.dyndns.org
+#EOHC END OF HEADER COMMENTS
 
 import asyncio
 from pyodide.http import pyfetch
