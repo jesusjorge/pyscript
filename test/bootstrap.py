533e3da1db207326a72bc284ed2036cd84cef74a
jesusjorge <35180978+jesusjorge@users.noreply.github.com>
2023-07-17 08:59:21 -0700

    Update bootstrap.py

--- test/bootstrap.py
+++ test/bootstrap.py
@@ -1,3 +1,8 @@
+#https://pyscript.net/examples/repl.html
+#
+# Al ejecutar este codigo en el REPL de PyScript, Python descarga el contenido de un URL, e inmediatamente ejecuta su codigo
+#
+
 import asyncio
 from pyodide.http import pyfetch
 async def main():
